d562bb8a861bd97362a62c585ded0adb1673efc4
Hank <horicky7@gmail.com>
2021-02-28 23:51:02 -0800

    base.html

--- toDoList/urls.py
+++ toDoList/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('', views.index, name='addevent'),
+    path('', views.index, name='login'),
+    path('', views.index, name='logout')
 ]
--- toDoList/views.py
+++ toDoList/views.py
@@ -3,5 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 
+def getNav():
+    nav = [{"label": "List View", "url": "index"},
+           {"label": "Event Type", "url": "index"},
+           {"label": "Add Event", "url": "index"},
+           {"label": "Login", "url": "index"},
+           {"label": "logout", "url": "index"}]
+    return nav
+
+
 def index(request):
-    return render(request, 'club/index.html')
+    return render(request, 'index.html', {"title": "list view", "navs": getNav()})
